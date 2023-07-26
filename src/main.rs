f20c896349af48aa8d7587cda4749bb25b7126d8
Shnob <jake.dombnz@gmail.com>
2023-07-26 22:34:48 +1200

    Some work from generating images

--- src/main.rs
+++ src/main.rs
@@ -12,33 +12,92 @@ use std::{env, path::Path, time::Instant};
 //    (255, 116, 0),
 //];
 
+//const COLOURS: [(u8, u8, u8); 3] = [
+//    // Deep blue
+//    (0, 33, 166),
+//    // Pink
+//    (250, 83, 252),
+//    // White
+//    (255, 255, 255),
+//];
+
 const COLOURS: [(u8, u8, u8); 3] = [
-    // Deep blue
-    (0, 33, 166),
+    // Yellow
+    (252, 252, 83),
     // Pink
     (250, 83, 252),
-    // White
-    (255, 255, 255),
+    // Pale cyan
+    (153, 255, 245),
 ];
+
+//const COLOURS: [(u8, u8, u8); 3] = [
+//    // Yellow
+//    (252, 252, 83),
+//    // Purple
+//    (127, 40, 250),
+//    // Cyan
+//    (52, 82, 201),
+//];
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let res: (u32, u32) = (args[1].parse().unwrap(), args[2].parse().unwrap());
-    let max: u64 = args[3].parse().unwrap();
+    let max: u64 = args[3].parse().unwrap_or_else(|_e| {
+        println!("No max value provided; Assuming 100");
+        100
+    });
+    let sampling = args[4].parse().unwrap_or_else(|_e| {
+        println!("No multi-sampling value provided; Assuming 1");
+        1
+    });
 
-    let target = (-1.99998588117, 0.);
-    let zoom = (2 as f64).powf(32.8);
+    // The Flower
+    //let target = (-1.99998588117, 0.);
+    //let zoom = (2 as f64).powf(32.8);
 
-    let mut final_image = RgbImage::new(res.0, res.1);
+    // Fluttershy Spiral
+    //let target = (-0.745628552, -0.166292177);
+    //let zoom = (2 as f64).powf(18.);
+
+    // The Star
+    //let target = (-1.94498538, 0.);
+    //let zoom = (2 as f64).powf(19.9);
+
+    // Void Spiral
+    let target = (-0.7765927806, 0.1366408558);
+    let zoom = (2 as f64).powf(29.9);
+
+    let mut rendered_image = RgbImage::new(res.0 * sampling, res.1 * sampling);
 
     let start_time = Instant::now();
 
-    generate_mandelbrot(&mut final_image, max, target, zoom);
+    generate_mandelbrot(&mut rendered_image, max, target, zoom);
 
     let time_taken = start_time.elapsed();
     let time_taken = time_taken.as_secs_f64();
 
     println!("Time taken: {time_taken}s");
+
+    let mut final_image = RgbImage::new(res.0, res.1);
+
+    let n = (sampling * sampling) as u16;
+    for x in 0..final_image.width() {
+        for y in 0..final_image.height() {
+            let mut col = (0, 0, 0);
+            for x in (sampling * x)..(sampling * x + sampling) {
+                for y in (sampling * y)..(sampling * y + sampling) {
+                    let pix = rendered_image.get_pixel(x, y);
+                    col = (
+                        col.0 + pix[0] as u16,
+                        col.1 + pix[1] as u16,
+                        col.2 + pix[2] as u16,
+                    );
+                }
+            }
+            col = (col.0 / n, col.1 / n, col.2 / n);
+            final_image.put_pixel(x, y, Rgb([col.0 as u8, col.1 as u8, col.2 as u8]));
+        }
+    }
 
     save_image(final_image);
 }
